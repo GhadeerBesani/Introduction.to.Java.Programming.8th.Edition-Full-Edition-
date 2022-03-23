694f66880f6e910536b78e8cd8496ea359dd3f0e
GhadeerBesani <43727883+GhadeerBesani@users.noreply.github.com>
2022-03-23 21:10:14 +0300

    Add files via upload

---
+++ PROGRAMMINGEXERCISES11.java
@@ -0,0 +1,21 @@
+/* (Display three messages) Write a program that displays Welcome to Java,
+   Welcome to Computer Science, and Programming is fun.
+*/
+package programming.exercises1.pkg1;
+
+/**
+ *
+ * @author Ghadeer
+ */
+public class PROGRAMMINGEXERCISES11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Computer Science");
+		System.out.println("Programming is fun");
+    }
+    
+}
