ed16c5d5b8744ad6e3d3f6fe4084d050445bc58a
GhadeerBesani <43727883+GhadeerBesani@users.noreply.github.com>
2022-03-23 21:50:14 +0300

    Add files via upload

---
+++ exercises_1_7.java
@@ -0,0 +1,13 @@
+/*
+(Approximating ) can be computed using the following formula:
+ */
+package programming;
+
+public class exercises_1_7 {
+
+
+    public static void main(String[] args) {
+     System.out.println(4 * (1.0 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9)-(1.0 / 11) + (1.0 /13 )));
+    }
+    
+}
