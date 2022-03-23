247a31be8dc349124587cc1f5b343485948560b1
GhadeerBesani <43727883+GhadeerBesani@users.noreply.github.com>
2022-03-23 22:35:11 +0300

    Add files via upload

---
+++ LISTING/ComputeExpression.java
@@ -0,0 +1,13 @@
+/*
+perform mathematical computations and display the result to the console
+ */
+
+
+
+public class ComputeExpression {
+
+    public static void main(String[] args) {
+        System.out.println((10.5 + 2 * 3) / (45 - 3.5));
+    }
+    
+}
---
+++ LISTING/WelcomeInMessageDialogBox.java
@@ -0,0 +1,18 @@
+/*
+ This application program displays Welcome to Java!
+ in a message dialog box.
+ 
+ */
+
+import javax.swing.JOptionPane;
+
+public class WelcomeInMessageDialogBox {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Welcome to Java!");
+    }
+    
+}
