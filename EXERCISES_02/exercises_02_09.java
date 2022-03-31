761658bbbf30d1dfcf9ceb43894a8dd043b2d643
GhadeerBesani <43727883+GhadeerBesani@users.noreply.github.com>
2022-03-31 03:27:25 -0700

    Add files via upload

---
+++ EXERCISES_02/exercises_02_09.java
@@ -0,0 +1,33 @@
+
+/*
+(Financial application: monetary units) Rewrite Listing 2.10, Compute-Change.java, to fix the possible loss of accuracy when converting a double value
+to an int value. Enter the input as an integer whose last two digits represent the
+cents. For example, the input 1156 represents 11 dollars and 56 cents.
+ */
+
+import java.util.Scanner;
+
+public class exercises_02_09 {
+
+
+    public static void main(String[] args) {
+        // Create a Scanner
+        Scanner input = new Scanner(System.in);
+        
+        // Receive the amount
+        System.out.print("Enter  an integer amount: ");
+        int amount = input.nextInt();
+        
+        // Find the number of one dollars
+        int numberOfOneDollars = amount / 100;
+        
+        // Find the number of one cents
+        int numberOfOnecents = amount % 100;
+        
+        // Display results
+        System.out.println("the input " + amount + " represents "
+                + numberOfOneDollars + " dollars and "
+                + numberOfOnecents + " cents");
+    }
+    
+}
