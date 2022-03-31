f1a67658b55d6916d62b8ef242c7f854fc550f04
GhadeerBesani <43727883+GhadeerBesani@users.noreply.github.com>
2022-03-31 03:36:50 -0700

    Add files via upload

---
+++ EXERCISES_02/exercises_02_18.java
@@ -0,0 +1,40 @@
+/*
+(Printing a table) Write a program that displays the following table:
+a 		b 		pow(a, b)
+1 		2 		1
+2 		3 		8
+3 		4 		81
+4 		5 		1024
+5 		6 		15625
+*/
+
+
+public class exercises_02_18 {
+
+
+    public static void main(String[] args) {
+		int a, b;
+		System.out.println("a        b        pow(a, b)");
+		a = 1;
+		b = 2;
+		System.out.println(a + "        " + b + 
+			"        " + (int)Math.pow(a, b));
+		a++;
+		b++;
+		System.out.println(a + "        " + b + 
+			"        " + (int)Math.pow(a, b));
+		a++;
+		b++;
+		System.out.println(a + "        " + b + 
+			"        " + (int)Math.pow(a, b));
+		a++;
+		b++;
+		System.out.println(a + "        " + b + 
+			"        " + (int)Math.pow(a, b));
+		a++;
+		b++;
+		System.out.println(a + "        " + b + 
+			"        " + (int)Math.pow(a, b));
+    }
+    
+}
