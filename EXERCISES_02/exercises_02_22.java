29c2b9297dd3aea8408e8d0249be1f0fa8009e39
GhadeerBesani <43727883+GhadeerBesani@users.noreply.github.com>
2022-03-31 03:40:15 -0700

    Add files via upload

---
+++ EXERCISES_02/exercises_02_22.java
@@ -0,0 +1,30 @@
+/*
+(Geometry: area of a hexagon) Write a program that prompts the user to enter the
+side of a hexagon and displays its area. The formula for computing the area of a
+hexagon is Area =(3√3 / 2)s²
+ */
+
+
+import java.util.Scanner;
+
+public class exercises_02_22 {
+
+
+    public static void main(String[] args) {
+        // Create new Scanner object
+        Scanner input = new Scanner(System.in);
+        
+        // Prompts the user to enter the side of a hexagon
+        System.out.println("Enter the side: ");
+        double hexagonSide = input.nextDouble();
+        
+        // Calculate the area of the hexagon
+        double area = 3 * Math.pow(3, 0.5) / 2 * Math.pow(hexagonSide, 2);
+        
+        // Displays the result
+        System.out.println("The area of the hexagon is " + area);
+        
+        
+    }
+    
+}
