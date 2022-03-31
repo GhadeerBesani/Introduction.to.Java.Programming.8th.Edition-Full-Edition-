a3556bdbdc434e6874e24b434712ec454002d5fc
GhadeerBesani <43727883+GhadeerBesani@users.noreply.github.com>
2022-03-31 02:45:22 -0700

    Add files via upload

---
+++ EXERCISES_02/exercises_02_03.java
@@ -0,0 +1,31 @@
+/*
+(Converting feet into meters) Write a program that reads a number in feet, converts it
+to meters, and displays the result. One foot is 0.305 meter. Here is a sample run:
+ */
+
+
+
+import java.util.Scanner;
+
+public class exercises_02_03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Create new Scanner object
+        Scanner input=new Scanner(System.in);
+        
+        // Reads a number in feet from the console
+        System.out.println("Enter a number in feet:");
+        double feet=input.nextDouble();
+        
+        // Convert feet into meters
+        double meters=0.305 * feet;
+        
+        // Display results
+	System.out.println(feet + " feet is "+ meters+ " meters");
+        
+    }
+    
+}
